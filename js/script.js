0d5945ee16e81b02b595b45d8ea7375352a808ff
Paulinha Severo <paulinhasevero13@gmail.com>
2017-12-21 16:28:36 -0200

    Atualizar Arquivos

--- js/script.js
+++ js/script.js
@@ -1,72 +1,78 @@
-addEventListener("load", function () {
-  setTimeout(hideURLbar, 0);
-}, false);
-
-function hideURLbar() {
-  window.scrollTo(0, 1);
-}
-
-
+// <!-- js -->
+<script src="js/jquery-2.2.3.min.js"></script>
+// <!--/js-->
+// <!-- //gallery -->
 <script src="js/jquery.tools.min.js"></script>
 <script src="js/jquery.mobile.custom.min.js"></script>
 <script src="js/jquery.cm-overlay.js"></script>
 
-
-$(document).ready(function () {
-  $('.cm-overlay').cmOverlay();
-});
-
-<!-- //gallery -->
-
+<script>
+  $(document).ready(function () {
+    $('.cm-overlay').cmOverlay();
+  });
+</script>
+// <!-- //gallery -->
+// <!--start-date-piker-->
 <link rel="stylesheet" href="css/jquery-ui.css" />
 <script src="js/jquery-ui.js"></script>
-
-
-$(function () {
-  $("#datepicker,#datepicker1").datepicker();
-});
-
+<script>
+  $(function () {
+    $("#datepicker,#datepicker1").datepicker();
+  });
+</script>
+// <!-- /End-date-piker -->
 <script type="text/javascript" src="js/move-top.js"></script>
 <script type="text/javascript" src="js/easing.js"></script>
 <script type="text/javascript">
-jQuery(document).ready(function ($) {
-  $(".scroll").click(function (event) {
-    event.preventDefault();
+  jQuery(document).ready(function ($) {
+    $(".scroll").click(function (event) {
+      event.preventDefault();
 
-    $('html,body').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000);
+      $('html,body').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000);
+    });
   });
-});
+</script>
+// <!--//end-smooth-scrolling-->
+// <!-- smooth-scrolling-of-move-up  -->
+<script type="text/javascript">
+  $(document).ready(function () {
+    /*
+    var defaults = {
+      containerID: 'toTop', // fading element id
+      containerHoverID: 'toTopHover', // fading element hover id
+      scrollSpeed: 1200,
+      easingType: 'linear'
+    };
+    */
 
+    $().UItoTop({
+      easingType: 'easeOutQuart'
+    });
 
-
-
-$(document).ready(function () {
-
-
-  $().UItoTop({
-    easingType: 'easeOutQuart'
   });
-
-});
+</script>
 
 
 <script src="js/SmoothScroll.min.js"></script>
 
-
-$(document).ready(function () {
-  size_li = $("#myList li").size();
-  x = 1;
-  $('#myList li:lt(' + x + ')').show();
-  $('#loadMore').click(function () {
-    x = (x + 1 <= size_li) ? x + 1 : size_li;
+<script>
+  $(document).ready(function () {
+    size_li = $("#myList li").size();
+    x = 1;
     $('#myList li:lt(' + x + ')').show();
+    $('#loadMore').click(function () {
+      x = (x + 1 <= size_li) ? x + 1 : size_li;
+      $('#myList li:lt(' + x + ')').show();
+    });
+    $('#showLess').click(function () {
+      x = (x - 1 < 0) ? 1 : x - 1;
+      $('#myList li').not(':lt(' + x + ')').hide();
+    });
   });
-  $('#showLess').click(function () {
-    x = (x - 1 < 0) ? 1 : x - 1;
-    $('#myList li').not(':lt(' + x + ')').hide();
-  });
-});
-
+</script>
+// <!-- Bootstrap core JavaScript
+//   ================================================== -->
+// <!-- Placed at the end of the document so the pages load faster -->
 <script src="js/bootstrap.js"></script>
